77a29444898b17632d7df3daa505369e8a9d984a
Ahmad Hammam <156898122+Ahmadhammam03@users.noreply.github.com>
2025-07-20 05:04:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.trainer import SAEExperiment
 experiment = SAEExperiment(data_path="data/", model_save_path="models/")
 
 # Run complete pipeline
-experiment.load_and_prepare_data(use_custom_csv=True)  # Uses YOUR CSV files
+experiment.load_and_prepare_data(use_custom_csv=True)
 experiment.initialize_model(encoding_layers=[20, 10])
 experiment.train_model(nb_epochs=200)
 experiment.evaluate_model()
